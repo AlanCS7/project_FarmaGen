de6cd62eb79270798bfa26c45c9b58bec4485b30
unknown <gui.albuquerque.gui@gmail.com>
2021-12-08 10:53:56 -0300

    @guiAdaCruz [Task: Criando a classe ProdutoController]

---
+++ src/main/java/com/Farmacia/FarmaGen/controller/ProdutoController.java
@@ -0,0 +1,64 @@
+package com.Farmacia.FarmaGen.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Classe responsável pelo CRUD
+ * 
+ * @author Guilherme Cruz
+ * @since 1.0
+ */
+
+@RestController
+@RequestMapping("/farmaGen/produtos")
+@CrossOrigin("*")
+public class ProdutoController {
+	
+	@Autowired
+	private ProdutoReository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<ProdutoModel>> GetAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProdutoModel> GetById(@PathVariable(value = "id") long id){
+		return repository.findById(id).map(resp -> ResponseEntity.status(200).body(resp))
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Id não Existe"));
+	}
+	
+	@GetMapping("/nome/{produto}")
+	public ResponseEntity<List<ProdutoModel>> GetByProduto(@PathVariable String produto){
+		return Response.ok(repository.findAllByNomeContainingIgnoreCase(produto));
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<ProdutoModel> post(@Valid @RequestBody ProdutoModel produto){
+		return ResponseEntity<ProdutoModel> post(@Valid @RequestBody ProdutoModel produto);			
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<ProdutoModel> put(@valid @RequestBody ProdutoModel produto){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable(value = "id") long id) {
+		repository.deleteById(id);
+	}
+}
