931b384d66b0aa99d4b1c2a706a8b400ae73c003
Kathleen-Fidelis <lucas.melo195@gmail.com>
2021-12-08 10:46:56 -0300

    @LucasCostaMelo [Task: Criando repository categoria]

---
+++ src/main/java/com/Farmacia/FarmaGen/repository/CategoriaRepository.java
@@ -0,0 +1,22 @@
+package com.Farmacia.FarmaGen.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Farmacia.FarmaGen.model.CategoriaModel;
+
+/**
+ * Interface responsável pela tabela Categoria, ela tem a finalidade de se
+ * conectar e manipular o banco de dados.
+ * 
+ * @author Lucas Melo
+ * @since 1.0
+ *
+ */
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
+	public List<CategoriaModel> findAllByCategoriaContainingIgnoreCase(String categoria);
+}
