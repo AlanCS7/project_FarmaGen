52898b552ddea9dd55ce57fe1736460cb14b03a7
Josiane <jcarolliny@gmail.com>
2021-12-08 10:55:01 -0300

    @JosianeCaroliny [Task: Criando Repository da tabela Produto]

---
+++ src/main/java/com/Farmacia/FarmaGen/repository/ProdutoRepository.java
@@ -0,0 +1,21 @@
+package com.Farmacia.FarmaGen.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+ /**
+ * 
+ * Interface responsável por se conectar e manipular o banco de dados.
+ * @author JosianeCaroliny
+ * @since 1.0
+ * 
+ */
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, Long>{
+	
+	public List<ProdutoModel> findAllByNomeContainingIgnoreCase (String nome);
+
+}
