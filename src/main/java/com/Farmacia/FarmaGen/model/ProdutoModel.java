b87229ce42d795975dc37327f991eb73512a63a7
AlanCS7 <alanclash2541@gmail.com>
2021-12-08 10:54:47 -0300

    @AlanCS7 [Task: Criação da ProdutoModel]

--- src/main/java/com/Farmacia/FarmaGen/model/ProdutoModel.java
+++ src/main/java/com/Farmacia/FarmaGen/model/ProdutoModel.java
@@ -11,7 +11,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.Farmacia.FarmaGen.model.CategoriaModel;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
